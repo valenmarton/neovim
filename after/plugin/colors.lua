2bafbcf8d14e96b0bf19a4346ce18f3331762bb0
valentinmarton <martonvalentin97@gmail.com>
2023-09-30 16:21:29 +0200

    remove telescope background color rose-pine

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -30,30 +30,6 @@ require('rose-pine').setup({
     -- headings = 'subtle'
   },
 
-  highlight_groups = {
-    ColorColumn = { bg = 'rose' },
-
-    -- Blend colours against the "base" background
-    CursorLine = { bg = 'foam', blend = 10 },
-    StatusLine = { fg = 'love', bg = 'love', blend = 10 },
-
-    -- By default each group adds to the existing config.
-    -- If you only want to set what is written in this config exactly,
-    -- you can set the inherit option:
-    Search = { bg = 'gold', inherit = false },
-    TelescopeBorder = { fg = "overlay", bg = "overlay" },
-    TelescopeNormal = { fg = "subtle", bg = "overlay" },
-    TelescopeSelection = { fg = "text", bg = "highlight_med" },
-    TelescopeSelectionCaret = { fg = "love", bg = "highlight_med" },
-    TelescopeMultiSelection = { fg = "text", bg = "highlight_high" },
-
-    TelescopeTitle = { fg = "base", bg = "love" },
-    TelescopePromptTitle = { fg = "base", bg = "pine" },
-    TelescopePreviewTitle = { fg = "base", bg = "iris" },
-
-    TelescopePromptNormal = { fg = "text", bg = "surface" },
-    TelescopePromptBorder = { fg = "surface", bg = "surface" },
-  },
 
 })
 
