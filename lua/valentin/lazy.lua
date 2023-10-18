39466c4779e037d1f0b5ba9178dec18cb660ada2
valentinmarton <martonvalentin97@gmail.com>
2023-10-18 15:58:48 +0200

    some changes :)

--- lua/valentin/lazy.lua
+++ lua/valentin/lazy.lua
@@ -93,15 +93,6 @@ require("lazy").setup({
     init = function() vim.g.mkdp_filetypes = { "markdown" } end
   },
   {
-    'f-person/git-blame.nvim',
-    keys = {
-      -- { "<leader>gbt", "<cmd>GitBlameToggle<cr>", desc = "GitBlameToggle" },
-      config = function()
-        require('gitblame').setup({ enabled = false })
-      end
-    }
-  },
-  {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
@@ -159,32 +150,32 @@ require("lazy").setup({
   { "stevearc/oil.nvim" },
   { "ggandor/leap.nvim" },
   { "karb94/neoscroll.nvim" },
-  -- {
-  --   "folke/flash.nvim",
-  --   event = "VeryLazy",
-  --   ---@type Flash.Config
-  --   opts = {},
-  --   -- stylua: ignore
-  --   keys = {
-  --     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
-  --     { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-  --     { "r", mode = "o",               function() require("flash").remote() end,     desc = "Remote Flash" },
-  --     {
-  --       "R",
-  --       mode = { "o", "x" },
-  --       function() require("flash").treesitter_search() end,
-  --       desc =
-  --       "Treesitter Search"
-  --     },
-  --     {
-  --       "<c-s>",
-  --       mode = { "c" },
-  --       function() require("flash").toggle() end,
-  --       desc =
-  --       "Toggle Flash Search"
-  --     },
-  --   },
-  -- },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o",               function() require("flash").remote() end,     desc = "Remote Flash" },
+      {
+        "R",
+        mode = { "o", "x" },
+        function() require("flash").treesitter_search() end,
+        desc =
+        "Treesitter Search"
+      },
+      {
+        "<c-s>",
+        mode = { "c" },
+        function() require("flash").toggle() end,
+        desc =
+        "Toggle Flash Search"
+      },
+    },
+  },
   {
     "nvimdev/lspsaga.nvim",
     dependencies = {
